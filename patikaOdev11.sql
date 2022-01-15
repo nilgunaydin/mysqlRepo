63fe0627aae9b019bfe0ccd06c53fb6d3b176805
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-01-15 19:13:40 +0300

    odev11

---
+++ patikaOdev11.sql
@@ -0,0 +1,31 @@
+
+use sakila;
+select first_name from actor
+union
+select first_name from customer;
+
+
+select first_name from actor
+intersect
+select first_name from customer;
+
+
+select first_name from actor
+except
+select first_name from customer;
+
+
+use sakila;
+select first_name from actor
+union all
+select first_name from customer;
+
+
+select first_name from actor
+intersect all
+select first_name from customer;
+
+
+select first_name from actor
+except all
+select first_name from customer;
